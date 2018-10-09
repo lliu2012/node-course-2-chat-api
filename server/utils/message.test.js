70a693770b168f822081d8320b7db9c36b8fa34f
Lei Liu <leiliu525@gmail.com>
2018-10-09 17:38:50 -0500

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,5 +9,34 @@ describe('generateMessage', () => {
     expect(resultObj.from).toBe('Admin');
     expect(resultObj.text).toBe('Hey');
     expect(resultObj.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const coords = {
+      latitude: 29.9000498,
+      longitude: -95.5806954
+    };
+
+    const from = 'Admin';
+    const url = `https://www.google.com/maps?q=${coords.latitude},${
+      coords.longitude
+    }`;
+
+    const resultObj = generateLocationMessage(
+      'Admin',
+      coords.latitude,
+      coords.longitude
+    );
+
+    // console.log(resultObj);
+
+    // expect(resultObj.from).toBe('Admin');
+    // expect(resultObj.url).toBe(
+    //   `https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`
+    // );
+    expect(resultObj.createdAt).toBeA('number');
+    expect(resultObj).toInclude({ from, url });
   });
 });
